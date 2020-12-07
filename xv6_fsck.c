e4741f34e978b37989347224c58c6084b9f16be2
Austen Vizcarra <vizcarra@royal-20.cs.wisc.edu>
2020-12-07 12:34:52 -0600

    65 of 120

--- xv6_fsck.c
+++ xv6_fsck.c
@@ -5,7 +5,13 @@
 #include <unistd.h>
 #include <assert.h>
 #include <sys/mman.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define stat xv6_stat  // avoid clash with host struct stat
+#define dirent xv6_dirent  // avoid clash with host struct stat
+#undef stat
+#undef dirent
 
 // -----copied in from fs.h-----------
 //Should verify the below comments are actually TRUE
@@ -59,22 +65,18 @@ struct dirent {
 };
 // -------end of stuff copied from fs.h--------
 
-//xv6 fs img
-//similar to vsfs very simple file system. There's no inode bitmap though. The inodes themselves
-// contain info about whether or not they are in use.
-// approx layout: somewhere on the image there's ...
-// superblock | inode table | bitmap (data) | data blocks
-// there are also some gaps in there
-
 int main(int argc, char *argv[]) {
 
   if (argc != 2) {
-    printf("usage: %s fs.img\n", argv[0]);
+    printf("Usage: xv6_fsck <file_system_image>.\n");
     return -1;
   }
 
-  int fd = open("fs.img", O_RDONLY);
-  assert(fd > -1);
+  int fd = open(argv[1], O_RDONLY);
+  if (fd == -1) {
+    fprintf(stderr, "image not found.\n");
+    exit(1);
+  }
 
   // how shoudl we read the sector?
   int rc;
@@ -90,24 +92,195 @@ int main(int argc, char *argv[]) {
   void *imgptr = mmap(NULL, sbuf.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
   assert(imgptr != MAP_FAILED);
 
-  // we need to find a way to tell the superblock pointer where the eff the superblock actually is.
   struct superblock *sb;
   sb = (struct superblock*) (imgptr + BSIZE); // set sb equal to the start of this buffer
-  printf("Image size (blocks): %d Data blocks: %d inodes: %d\n", sb->size, sb->nblocks, sb->ninodes); // will print first three fields of the sb
+  //printf("Image size (blocks): %d Data blocks: %d inodes: %d\n", sb->size, sb->nblocks, sb->ninodes); // will print first three fields of the sb
+  
   //mmap is great because now we have this pointer that means we don't have to read and seek.
   // we can just treat the file as if it is in memory at this point and mmap handles everything else.
 
-  //next thing to do is cast s struct dinode to point to the first inode!
-  int i;
+  //next thing to do is cast as struct dinode to point to the first inode
   struct dinode* dip = (struct dinode *) (imgptr + (2*BSIZE)); //(since we skip first two blocks)
-  for (i = 0; i < sb->ninodes; ++i) {
-    printf("%d type: %d\n", i, dip->type);
-    dip++;
-  }
+  //test print of inodes
+  // for (int i = 0; i < sb->ninodes; ++i) {
+  //   printf("%d type: %d size %d addrs: ", i, dip[i].type, dip[i].size);  
+  //   for (int j = 0; j < NDIRECT+1; ++j) {
+  //     printf("%u ", dip[i].addrs[j]);
+  //   } 
+  //   printf ("\n");
+  // }
 
   // next thing is to figure out where the bitmap is
+  //Unused | Superblock | Inodes ... | Unused | Bitmap | Data ...
+  // 3*BSIZE to skip unused, sb, unused; sb->ninodes/IPB to get number of blocks;
+  // then multiply that by BSIZE to advance pointer n blocks.
+  
+  unsigned char* bitmap = (unsigned char *) (imgptr + (3*BSIZE) + ((sb->ninodes/IPB) * BSIZE));
 
-  // then do other stuff (rest of p5)
+  //to make things easier, going to store the bitmap into an array
+  int bitmap_arr[sb->size];
+  int charCt = 0;
+  int count = 0;
+  for (int i = 0; i < sb->size; ++i) {
+    char x = bitmap[charCt];
+    int mask = 1 << count;
+    bitmap_arr[i] = (mask & x) >> (count);
+    ++count;
+    if (count == 8) {
+      ++charCt;
+      count = 0;
+    }
+  }
+  
+  //printf("bitmap = %p\n", bitmap);
+  //int num_bits_bitmap = sb->nblocks + sb->ninodes/IPB + 3;
+  //int bitmap_size = (sb->nblocks + sb->ninodes/IPB + sb->nblocks/BPB + 4) / 8;
+  //int num_blocks_bitmap = (sb->size / (BPB)) ;//+ 1;
+
+  // printf("num bits in bitmap = %d\n", num_bits_bitmap);
+  // printf("bitmap size = %d\n", bitmap_size);
+  // printf("num blocks bitmap = %d\n", num_blocks_bitmap);
+
+  // finally get pointer to where the data lives. Assuming here that the bitmap
+  // is 1 block based on sb->size / (512 * 8) + 1.
+  void* dataptr = (void *) (imgptr + (4*BSIZE) + ((sb->ninodes/IPB) * BSIZE));
+  //printf("dataptr = %p\n", dataptr);
+
+  // check that all inodes = 0 1 2 or 3
+  for (int i = 0; i < sb->ninodes; ++i) {
+    if (dip[i].type < 0 || dip[i].type > 3) {
+      // error time
+      fprintf(stderr, "ERROR: bad inode.\n");
+      exit(1);
+    }
+  }
+
+  int first_db = (dataptr - imgptr) / BSIZE;
+  int last_db = sb->nblocks + first_db; // should this be -1? Not sure if count starts at 0.
+  //printf("first %d last %d\n", first_db, last_db);
+  // looks like the addresses stored in dip are actually just offsets
+  // find the in use inodes. check address is valid - points to valid datablock address within the image
+  for (int i = 0; i < sb->ninodes; ++i) {
+    if (dip[i].type > 0) {
+      for (int j = 0; j < NDIRECT; ++j) {// might need to iterate through the indirect guy too
+        // if the block is used and invalid, print an error
+        if (dip[i].addrs[j] != 0 && (dip[i].addrs[j] < first_db || 
+          dip[i].addrs[j] > last_db)) {
+          fprintf(stderr, "ERROR: bad direct address in inode.\n");
+          exit(1);
+        }
+      }
+      // now do the indirect check
+      if (dip[i].addrs[NDIRECT] != 0 && (dip[i].addrs[NDIRECT] < first_db || dip[i].addrs[NDIRECT] > last_db)) {
+        fprintf(stderr, "ERROR: bad indirect address in inode.\n");
+        exit(1);
+      }
+    }
+  }
+
+  // 3. Root directory exists, its inode number is 1, and the parent of the root directory is itself. 
+  // ERROR: root directory does not exist.
+  // I think root is always at 29...
+  struct dirent *de = (struct dirent *) (imgptr + (29*BSIZE));
+  // check the dot
+  if (dip[1].type != 1 || strcmp(de[0].name, ".") != 0 || de[0].inum != 1) {
+    fprintf(stderr, "ERROR: root directory does not exist.\n");
+    exit(1);
+  }
+  // check the dot dot
+  if (dip[1].type != 1 || strcmp(de[1].name, "..") != 0 || de[1].inum != 1) {
+    fprintf(stderr, "ERROR: root directory does not exist.\n");
+    exit(1);
+  }
+
+  // 4. Each directory contains . and .. entries, and the . entry points to the directory itself. 
+  // ERROR: directory not properly formatted.\n
+  for (int i = 0; i < sb->ninodes; ++i) {
+    if (dip[i].type == 1) { // means it is a directory
+      struct dirent *deCheck = (struct dirent *) (imgptr + (dip[i].addrs[0] * BSIZE));
+        if (strcmp(deCheck[0].name, ".") != 0 || deCheck[0].inum != i || strcmp(deCheck[1].name, "..") != 0) {
+          fprintf(stderr, "ERROR: directory not properly formatted.\n");
+          exit(1);
+        }
+    }
+  }
+
+  // 5. For in-use inodes, each address in use is also marked in use in the bitmap. 
+  // ERROR: address used by inode but marked free in bitmap.\n
+  // also going to catch all these in use addresses in an array
+  int used_addr[sb->size]; // hope this sets them all to zero initially
+  // initialize evryting to zero to be safe
+  for (int i = 0; i < sb->size; ++i) {
+    used_addr[i] = 0;
+  }
+  //set the first 28 to 1 since that should always be true more or less.
+  for (int i = 0; i < 29; ++i) {
+    used_addr[i] = 1;
+  }
+
+  for (int i = 0; i < sb->ninodes; ++i) {
+    if (dip[i].type > 0) { // means the inode is in use
+      for (int j = 0; j < NDIRECT; ++j) {
+        if (dip[i].addrs[j] != 0) {
+          used_addr[dip[i].addrs[j]] = 1; // set this address to used.
+          unsigned char bitmap_byte = *(bitmap + (dip[i].addrs[j] / 8));
+          //printf("dip i addrs j = %u\n", dip[i].addrs[j]);
+          //printf("bitmap_byte = %x\n", bitmap_byte);
+          int bitmap_pos = dip[i].addrs[j] % 8;
+          //printf("bitmap_pos = %d\n", bitmap_pos);
+          unsigned char final_bit = (bitmap_byte >> bitmap_pos);
+          final_bit = final_bit << 7;
+          //final_bit = final_bit >> 7;
+          //printf("final bit = %u\n", final_bit);
+          if (final_bit == 0) {
+            fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
+            exit(1);
+          }
+        }
+
+      }
+      // now check indirect
+      if (dip[i].addrs[NDIRECT] != 0) {
+        used_addr[dip[i].addrs[NDIRECT]] = 1;
+        // per bitmap help slide I think indirect address points to an entire block of addresses
+        uint* indir_addrs = (uint *) (imgptr + (dip[i].addrs[NDIRECT] * BSIZE));
+        // number uints in a block = 512 / 8 or BSIZE/8
+        for (int i = 0; i < BSIZE/sizeof(uint); ++i) {
+          if (indir_addrs[i] != 0) {
+            used_addr[indir_addrs[i]] = 1;
+            unsigned char bitmap_byte = *(bitmap + (indir_addrs[i] / 8));
+            int bitmap_pos = indir_addrs[i] % 8;
+            unsigned char final_bit = (bitmap_byte >> bitmap_pos);
+            final_bit = final_bit << 7;
+            
+            if (final_bit == 0) {
+              //printf("testing...\n");
+              //printf("final_bit = %d\n", (int)final_bit);
+              fprintf(stderr, "ERROR: address used by inode but marked free in bitmap.\n");
+              exit(1);
+            }
+          }
+        }
+
+      }
+      
+    }
+  }
+
+  // 6. For blocks marked in-use in bitmap, actually is in-use in an inode or indirect block somewhere. 
+  // ERROR: bitmap marks block in use but it is not in use.\n
+  for (int i = 0; i < sb->nblocks; ++i) {
+    //printf("bitmap_arr[%d] = %d\n", i, bitmap_arr[i]);
+    if (bitmap_arr[i] == 1 && used_addr[i] != 1) {
+      fprintf(stderr, "ERROR: bitmap marks block in use but it is not in use.\n");
+      exit(1);
+    }
+  }
+  
+
+
+
+
 
   return 0;
 }
